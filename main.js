07b8f53eda9eb5cbb6ccc4e3f683524be569c701
Oleksii Kurii bc2025-2 <olekseykuriy@gmail.com>
2025-09-25 20:53:11 +0300

    Completed part 3

---
+++ main.js
@@ -0,0 +1,9 @@
+const currency = "JPY";
+const exact_date = "20240127"; 
+
+const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+
+url.searchParams.append("valcode", currency);
+url.searchParams.append("date", exact_date);
+
+console.log(url.toString());
